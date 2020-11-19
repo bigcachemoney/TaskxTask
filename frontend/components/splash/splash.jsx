ee9670202aff8c80f9b2da5d8d2b0f981dedd214
angrysoftwareengineer88@gmail.com <raylan.liang@gmail.com>
2020-11-19 09:35:52 -0500

    added css to splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,21 +4,36 @@ import { Link } from 'react-router-dom';
 const Splash = () => {
   return (
     <div className="main-splash">
-     
-
       <div className="wallpaper">
-
         <div className="topnav">
           <Link to="/"><img className="logo" src="https://i.imgur.com/7FP232c.png"/></Link>
 
           <div id="buttons">
-              <Link id="login" to="/login">Login</Link>
-              <Link id="signup" to="/signup">Sign Up</Link>
+              <Link id="login" to="/login">LOGIN</Link>
+              <Link id="signup" to="/signup">SIGN UP</Link>
           </div>
-
         </div>
 
+        <div className="text-container">
+        
+            <h1 className="text-header">FIND THE BEST HELP</h1>
+            <p className="text-body">GET HELP FROM THOUSANDS OF TRUSTED TASKERS FOR EVERYTHING FROM ERRANDS TO DEFEATING DUNGEON BOSSES!</p>
+          
+            
+              <div className="search-padding">
+                  <div className="search-form">
+
+                      <div className="searchbox">
+                        <input type="search" id="search" placeholder="I need help with..." />
+                        <Link id="searchbutton" to="/login">GET HELP TODAY</Link>
+                      </div>
+                  </div>
+              </div>
+        </div>
+        
       </div>
+
+
     </div>
   )
 }
